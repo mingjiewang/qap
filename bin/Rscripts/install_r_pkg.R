47e8bade18234b23594356ad38447dff65278c0b
Jianfeng Li <lee_jianfeng@sjtu.edu.cn>
2017-10-19 18:40:18 +0800

    Beta 2.2

--- bin/Rscripts/install_r_pkg.R
+++
@@ -1,5 +0,0 @@
-source("https://bioconductor.org/biocLite.R")
-options(repos = c(CRAN="http://cran.r-project.org"))
-biocLite("Biostrings", ask = FALSE)
-install.packages(c("gplots", "RColorBrewer", "ggplot2", "seqinr", "optparse", "plyr", 
-                 "xlsx", "stringr", "scatterplot3d", "ggpubr"))
