e8a3865e63e2a8edcfff0680f96fd4ed365d6018
XU, Nuo <49825669+XU-Nuo@users.noreply.github.com>
2021-05-11 15:29:50 +0800

    depdency fix for install-r-pkg

--- bin/Rscripts/install_r_pkg.R
+++ bin/Rscripts/install_r_pkg.R
@@ -2,4 +2,4 @@ source("http://bioconductor.org/biocLite.R")
 options(repos = c(CRAN="http://cran.r-project.org"))
 biocLite("Biostrings", ask = FALSE)
 install.packages(c("gplots", "RColorBrewer", "ggplot2", "seqinr", "optparse", "plyr", 
-                 "xlsx", "stringr", "scatterplot3d", "ggpubr", "futile.logger"))
+                 "xlsx", "stringr", "scatterplot3d", "ggpubr", "futile.logger"), dependencies = TRUE)
