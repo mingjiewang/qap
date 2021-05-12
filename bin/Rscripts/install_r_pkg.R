415d266973238d1f5a6fe7731552d9aa7c3183c6
XU, Nuo <49825669+XU-Nuo@users.noreply.github.com>
2021-05-12 11:51:17 +0800

    Update install_r_pkg.R

--- bin/Rscripts/install_r_pkg.R
+++ bin/Rscripts/install_r_pkg.R
@@ -3,3 +3,35 @@ options(repos = c(CRAN="http://cran.r-project.org"))
 biocLite("Biostrings", ask = FALSE)
 #install.packages(c("gplots", "RColorBrewer", "ggplot2", "seqinr", "optparse", "plyr", 
 #                "xlsx", "stringr", "scatterplot3d", "ggpubr", "futile.logger"), dependencies = TRUE)
+print("===Installing gplots===")
+install.packages("gplots", dependencies = TRUE) 
+
+print("===Installing RColorBrewer===")
+install.packages("RColorBrewer", dependencies = TRUE) 
+
+print("===Installing ggplot2===")
+install.packages("ggplot2", dependencies = TRUE) 
+
+print("===Installing seqinr===")
+install.packages("seqinr", dependencies = TRUE) 
+
+print("===Installing optparse===")
+install.packages("optparse", dependencies = TRUE) 
+
+print("===Installing plyr===")
+install.packages("plyr", dependencies = TRUE) 
+
+print("===Installing xlsx===")
+install.packages("xlsx", dependencies = TRUE) 
+
+print("===Installing stringr===")
+install.packages("stringr", dependencies = TRUE)
+
+print("===Installing scatterplot3d===")
+install.packages("scatterplot3d", dependencies = TRUE) 
+
+print("===Installing ggpubr===")
+install.packages("ggpubr", dependencies = TRUE)
+
+print("===Installing futile.logger===")
+install.packages("futile.logger", dependencies = TRUE) 
