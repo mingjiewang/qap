b64eda29008ea13c2e9665d82aa2f565a49d63b7
XU, Nuo <49825669+XU-Nuo@users.noreply.github.com>
2021-05-17 02:30:49 -0400

    GitHub Action pipeline for docker image building

--- bin/Rscripts/install_r_pkg.R
+++ bin/Rscripts/install_r_pkg.R
@@ -1,5 +1,58 @@
-source("https://bioconductor.org/biocLite.R")
+print("===Installing Bioconductor===")
+#source("https://bioconductor.org/biocLite.R")
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+BiocManager::install()
 options(repos = c(CRAN="http://cran.r-project.org"))
-biocLite("Biostrings", ask = FALSE)
-install.packages(c("gplots", "RColorBrewer", "ggplot2", "seqinr", "optparse", "plyr", 
-                 "xlsx", "stringr", "scatterplot3d", "ggpubr"))
+#setRepositories(ind = c(1:9))
+print("===Installing Biostrings===")
+#biocLite("Biostrings", ask = FALSE)
+BiocManager::install("Biostrings", ask = FALSE)
+
+print("===Installing shiny===")
+install.packages("shiny", dependencies = TRUE) 
+
+print("===Installing systemfonts===")
+install.packages("systemfonts", dependencies = TRUE) 
+
+print("===Installing devtools===")
+install.packages("devtools", dependencies = TRUE) 
+require(devtools)
+install_version("caTools", version = "1.17.1", repos = "http://cran.us.r-project.org")
+
+print("===Installing ape===")
+install.packages("ape", dependencies = TRUE) 
+
+print("===Installing gplots===")
+install.packages("gplots", dependencies = TRUE) 
+
+print("===Installing RColorBrewer===")
+install.packages("RColorBrewer", dependencies = TRUE) 
+
+print("===Installing ggplot2===")
+install.packages("ggplot2", dependencies = TRUE) 
+
+print("===Installing seqinr===")
+install.packages("seqinr", dependencies = TRUE) 
+
+print("===Installing optparse===")
+install.packages("optparse", dependencies = TRUE) 
+
+print("===Installing plyr===")
+install.packages("plyr", dependencies = TRUE) 
+
+print("===Installing xlsx===")
+install.packages("xlsx", dependencies = TRUE) 
+
+print("===Installing stringr===")
+install.packages("stringr", dependencies = TRUE)
+
+print("===Installing scatterplot3d===")
+install.packages("scatterplot3d", dependencies = TRUE) 
+
+print("===Installing ggpubr===")
+install.packages("ggpubr", dependencies = TRUE)
+
+print("===Installing futile.logger===")
+install.packages("futile.logger", dependencies = TRUE) 
+
